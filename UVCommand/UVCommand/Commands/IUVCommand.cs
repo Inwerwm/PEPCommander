001bde8e627e48506227ab9fe836b0cd9ff400ba
Inwerwm <saclainvervm@gmail.com>
2021-01-29 23:28:25 +0900

    要素を追加

--- UVCommand/UVCommand/Commands/IUVCommand.cs
+++ UVCommand/UVCommand/Commands/IUVCommand.cs
@@ -8,6 +8,21 @@ namespace UVCommand.Commands
 {
     public interface IUVCommand
     {
+        /// <summary>
+        /// コマンド名
+        /// </summary>
+        string Name { get; }
+        /// <summary>
+        /// 説明
+        /// </summary>
+        string Description { get; }
+        /// <summary>
+        /// 実行
+        /// </summary>
         void Do();
+        /// <summary>
+        /// もとに戻す
+        /// </summary>
+        void UnDo();
     }
 }
