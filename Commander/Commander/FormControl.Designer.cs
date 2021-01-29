a4e090372b20a060ebc7775554b7e53a0c6dc03b
Inwerwm <saclainvervm@gmail.com>
2021-01-30 00:19:09 +0900

    GUIを作成

--- Commander/Commander/FormControl.Designer.cs
+++ Commander/Commander/FormControl.Designer.cs
@@ -29,19 +29,187 @@ namespace PEPCommander
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBoxCommands = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.listBoxMacros = new System.Windows.Forms.ListBox();
+            this.listBoxRequires = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelRequire = new System.Windows.Forms.Label();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // listBoxCommands
+            // 
+            this.listBoxCommands.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxCommands.FormattingEnabled = true;
+            this.listBoxCommands.ItemHeight = 12;
+            this.listBoxCommands.Location = new System.Drawing.Point(3, 3);
+            this.listBoxCommands.Name = "listBoxCommands";
+            this.tableLayoutPanel1.SetRowSpan(this.listBoxCommands, 2);
+            this.listBoxCommands.Size = new System.Drawing.Size(264, 352);
+            this.listBoxCommands.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAdd.Location = new System.Drawing.Point(273, 154);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(44, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = ">>";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRemove.Location = new System.Drawing.Point(273, 183);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(44, 23);
+            this.buttonRemove.TabIndex = 1;
+            this.buttonRemove.Text = "<<";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 185F));
+            this.tableLayoutPanel1.Controls.Add(this.listBoxCommands, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonRemove, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonRun, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonAdd, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listBoxMacros, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxDescription, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 65F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 426);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // listBoxMacros
+            // 
+            this.listBoxMacros.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxMacros.FormattingEnabled = true;
+            this.listBoxMacros.ItemHeight = 12;
+            this.listBoxMacros.Location = new System.Drawing.Point(323, 3);
+            this.listBoxMacros.Name = "listBoxMacros";
+            this.tableLayoutPanel1.SetRowSpan(this.listBoxMacros, 2);
+            this.listBoxMacros.Size = new System.Drawing.Size(264, 352);
+            this.listBoxMacros.TabIndex = 0;
+            // 
+            // listBoxRequires
+            // 
+            this.listBoxRequires.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxRequires.FormattingEnabled = true;
+            this.listBoxRequires.ItemHeight = 12;
+            this.listBoxRequires.Location = new System.Drawing.Point(3, 23);
+            this.listBoxRequires.Name = "listBoxRequires";
+            this.listBoxRequires.Size = new System.Drawing.Size(174, 328);
+            this.listBoxRequires.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.listBoxRequires, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.labelRequire, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(593, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel1.SetRowSpan(this.tableLayoutPanel2, 2);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(180, 354);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // labelRequire
+            // 
+            this.labelRequire.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelRequire.AutoSize = true;
+            this.labelRequire.Location = new System.Drawing.Point(3, 4);
+            this.labelRequire.Name = "labelRequire";
+            this.labelRequire.Size = new System.Drawing.Size(77, 12);
+            this.labelRequire.TabIndex = 1;
+            this.labelRequire.Text = "要求選択要素";
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRun.Location = new System.Drawing.Point(593, 363);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(180, 60);
+            this.buttonRun.TabIndex = 2;
+            this.buttonRun.Text = "実行";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.AcceptsReturn = true;
+            this.textBoxDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.textBoxDescription, 3);
+            this.textBoxDescription.Location = new System.Drawing.Point(3, 363);
+            this.textBoxDescription.Multiline = true;
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.ReadOnly = true;
+            this.textBoxDescription.Size = new System.Drawing.Size(584, 60);
+            this.textBoxDescription.TabIndex = 3;
             // 
             // FormControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "FormControl";
             this.Text = "Commander";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxCommands;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ListBox listBoxRequires;
+        private System.Windows.Forms.Label labelRequire;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ListBox listBoxMacros;
+        private System.Windows.Forms.TextBox textBoxDescription;
     }
 }
