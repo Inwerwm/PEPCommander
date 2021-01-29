7ab210c864ff0aefcb0316415d257e2327d2b5ce
Inwerwm <saclainvervm@gmail.com>
2021-01-29 22:37:25 +0900

    プロジェクト管理を開始

---
+++ UVCommand/UVCommand/Commands/UVCLocalInvertVertical.cs
@@ -0,0 +1,28 @@
+﻿using PEPlugin.Pmx;
+using PEPExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UVCommand.Commands
+{
+    class UVCLocalInvertVertical : IUVCommand
+    {
+        IEnumerable<IPXFace> TargetFaces { get; }
+        IEnumerable<VertexWithFace> TargetVertices { get; }
+
+        public UVCLocalInvertVertical(IEnumerable<IPXFace> targetFaces)
+        {
+            TargetFaces = targetFaces;
+
+            TargetVertices = TargetFaces.SelectMany(face => face.ToVertices().Select(vtx => new VertexWithFace(vtx, face)));
+        }
+
+        public void Do()
+        {
+
+        }
+    }
+}
