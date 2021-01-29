f795b3708c5ca78921ac6b5a7e733f035ca53fd5
Inwerwm <saclainvervm@gmail.com>
2021-01-30 01:08:35 +0900

    コマンド管理クラスを追加

---
+++ Commander/Commander/CommandManager.cs
@@ -0,0 +1,18 @@
+﻿using PEPCommander.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEPCommander
+{
+    public class CommandManager
+    {
+        public IReadOnlyList<ICommand> List => new ICommand[]
+        {
+            new UVCLocalInvertVertical(),
+            new UVCLocalInvertHorizontal()
+        };
+    }
+}
